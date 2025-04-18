b9153405b0290d39ec2c577d74e3df9b2a677924
ramarojaona <144231899+Yves-MG@users.noreply.github.com>
2025-04-18 13:13:32 +0200

    first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Gestion.Application.Dtos.Users.Request;
+using Gestion.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gestion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] Register registerDto)
+        {
+            var result = await _authService.RegisterAsync(registerDto);
+
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok(result.Message);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] Login loginDto)
+        {
+            var result = await _authService.LoginAsync(loginDto);
+            if (result == null)
+                return Unauthorized("Email ou mot de passe incorrect.");
+
+            return Ok(result.Data);
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken([FromBody] string refreshToken)
+        {
+            var result = await _authService.RefreshTokenAsync(refreshToken);
+            if (!result.IsSuccess)
+            {
+                return Unauthorized(result.Message);
+            }
+            return Ok(result.Data);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+using System.Text;
+using Gestion.Core.Entities;
+using Gestion.Core.Services.Implementations;
+using Gestion.Core.Services.Interfaces;
+using Gestion.Infrastructure;
+using Gestion.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+//configuration de la base de données
+builder.Services.AddDbContext<GestionDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sql =>
+        {
+            sql.MigrationsAssembly("Gestion.Infrastructure");
+            //sql.EnableRetryOnFailure();
+        });
+});
+
+//configuration de l'identité
+builder.Services.AddIdentity<User, Role>()
+       .AddRoles<Role>()
+       .AddEntityFrameworkStores<GestionDbContext>() //stockage des données dans la base de données
+       .AddDefaultTokenProviders(); //ajout des jetons par défaut
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+//builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
+
+//.AddDefaultTokenProviders();
+//configuration jwt
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
+// Add services to the container.
+
+var app = builder.Build();
+// Initialisation des rôles au démarrage de l'application
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<Role>>();
+    await SeedRole.Initialize(scope.ServiceProvider, roleManager);
+}
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
